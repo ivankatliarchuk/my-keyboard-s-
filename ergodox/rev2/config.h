37244b53b615720c74e30a1d90d75ddae17dcb00
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2020-07-18 12:01:56 +0100

    should just work

--- ergodox/rev2/config.h
+++ ergodox/rev2/config.h
@@ -2,6 +2,7 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
+
 #define ORYX_CONFIGURATOR
 #undef DEBOUNCE
 #define DEBOUNCE 40
