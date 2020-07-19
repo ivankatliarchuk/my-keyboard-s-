61fe0d8248a9021ef18999404aab8930634a66df
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2020-07-19 20:36:37 +0100

    stabilize double bounce

--- ergodox/rev2/config.h
+++ ergodox/rev2/config.h
@@ -7,6 +7,12 @@
 #undef DEBOUNCE
 #define DEBOUNCE 45
 
+#undef TAPPING_TERM
+#define TAPPING_TERM 237
+
 #define PERMISSIVE_HOLD
+#define TAPPING_FORCE_HOLD
+#define NO_AUTO_SHIFT_SPECIAL
+#define NO_AUTO_SHIFT_ALPHA
 
 #define FIRMWARE_VERSION u8"lAN7e/jaalw"
